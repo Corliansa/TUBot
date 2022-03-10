687292839375f8cd6c24395eea84c0a683fab9dc
Corliansa Kusumah <corliansa@gmail.com>
2022-03-10 18:32:37 +0100

    some fix

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # TUBot
+
 A bot to check TU Berlin services. To deploy your own, follow these steps:
+
 1. run `git clone https://github.com/Corliansa/TUBot`
 2. run `yarn` or `npm install`
 3. configure your enviroment variables for TELEGRAM_KEY (usually using .env file)
-4. after all dependencies are downloaded, run `yarn dev` or `npm run dev` to start a dev server
+4. after all dependencies are downloaded, run `yarn dev` or `npm run dev` to test it
 5. for production, run `yarn start` or `npm run start`
 
 Use @BotFather in telegram to create your own bot and get the TELEGRAM_KEY.
 
-Currently, it is being deployed using heroku to @TUOnlineBot in telegram. Feel free to add the bot to your group!
+Currently, it is being deployed to @TUOnlineBot in telegram. Feel free to add the bot to your group!
--- index.js
+++ index.js
@@ -8,14 +8,40 @@ const token = process.env.TELEGRAM_KEY;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
+const onlineText = "Online ✅";
+const offlineText = "Offline ❌";
+
+const getStatus = (on) => {
+	return on ? onlineText : offlineText;
+};
+
 const isOnline = async (url) => {
 	try {
-		const res = await axios.get(url);
+		const res = await axios.get(url, { timeout: 5000 });
 		return res;
 	} catch (err) {
-		// console.error(err);
 		return err.response;
 	}
+};
+
+const isis = async () => {
+	const res = await isOnline("https://isis.tu-berlin.de");
+	const ok = res?.data?.includes("Willkommen auf ISIS") && res?.status === 200;
+	return ok;
+};
+
+const shib = async () => {
+	const res = await isOnline(
+		"https://shibboleth.tubit.tu-berlin.de/idp/profile/SAML2/Redirect/SSO"
+	);
+	const ok = res?.data?.includes("Web Login Service - Stale Request");
+	return ok;
+};
+
+const moses = async () => {
+	const res = await isOnline("https://moseskonto.tu-berlin.de");
+	const ok = res?.data?.includes("Willkommen bei Moses") && res?.status === 200;
+	return ok;
 };
 
 bot.onText(/\/start/, (msg, match) => {
@@ -65,52 +91,43 @@ bot.onText(/\/debug/, (msg, match) => {
 
 bot.onText(/\/isis/, async (msg, match) => {
 	const chatId = msg.chat.id;
-	const res = await isOnline("https://isis.tu-berlin.de");
-	const ok = res?.data.includes("Willkommen auf ISIS") && res?.status === 200;
-	bot.sendMessage(chatId, `ISIS is ${ok ? "online" : "offline"}`);
+	bot.sendMessage(chatId, `ISIS is ${getStatus(isis())}`);
 });
 
 bot.onText(/\/shib/, async (msg, match) => {
 	const chatId = msg.chat.id;
-	const res = await isOnline(
-		"https://shibboleth.tubit.tu-berlin.de/idp/profile/SAML2/Redirect/SSO"
-	);
-	const ok = res?.data.includes("Web Login Service - Stale Request");
-	bot.sendMessage(chatId, `Shibboleth is ${ok ? "online" : "offline"}`);
+	bot.sendMessage(chatId, `Shibboleth is ${getStatus(shib())}`);
 });
 
 bot.onText(/\/moses/, async (msg, match) => {
 	const chatId = msg.chat.id;
-	const res = await isOnline("https://moseskonto.tu-berlin.de");
-	const ok = res?.data.includes("Willkommen bei Moses") && res?.status === 200;
-	bot.sendMessage(chatId, `Moses is ${ok ? "online" : "offline"}`);
+	bot.sendMessage(chatId, `Moses is ${getStatus(moses())}`);
 });
 
-// bot.onText(/\/autolab/, async (msg, match) => {
-// 	const chatId = msg.chat.id;
-// 	const res = await isOnline("https://autolab.service.tu-berlin.de");
-// 	console.log(res);
-// 	bot.sendMessage(chatId, `${res}`);
-// });
+bot.onText(/\/check (.+)/, async (msg, match) => {
+	const chatId = msg.chat.id;
+	const resp = match[1];
+	axios
+		.get(resp, {
+			timeout: 5000,
+		})
+		.then(function (response) {
+			console.log(response);
+			bot.sendMessage(chatId, getStatus(true));
+		})
+		.catch(function (error) {
+			console.error(error);
+			bot.sendMessage(chatId, `${getStatus(false)}: ${error?.message}`);
+		});
+});
 
 bot.onText(/\/all/, async (msg, match) => {
 	const chatId = msg.chat.id;
-	const res = await isOnline("https://isis.tu-berlin.de");
-	const isis = res?.data.includes("Willkommen auf ISIS") && res?.status === 200;
-	const res2 = await isOnline(
-		"https://shibboleth.tubit.tu-berlin.de/idp/profile/SAML2/Redirect/SSO"
-	);
-	const shib = res2?.data.includes("Web Login Service - Stale Request");
-	const res3 = await isOnline("https://moseskonto.tu-berlin.de");
-	const moses =
-		res3?.data.includes("Willkommen bei Moses") && res3?.status === 200;
-	// const res4 = await isOnline("https://autolab.service.tu-berlin.de");
-	// const autolab = res4?.status === 200;
 	bot.sendMessage(
 		chatId,
 		`Checking all services:
-	ISIS is ${isis ? "online" : "offline"}
-	Shibboleth is ${shib ? "online" : "offline"}
-	Moses is ${moses ? "online" : "offline"}`
+	ISIS is ${getStatus(isis())}
+	Shibboleth is ${getStatus(shib())}
+	Moses is ${getStatus(moses())}`
 	);
 });
